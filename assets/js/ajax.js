2584724fc019cf0f286e40a4f30e5a8ed8a5879d
Isabella Sturm <ixs1022@rit.edu>
2018-01-27 14:04:36 -0500

    added example of how to call function

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -2,6 +2,7 @@
  * AJAX function to get information from API
  * two parameters: t, type of request - get or post
  * 				   d, path to get to data in API
+ * Example: zFLT('get',{'path':'/[path]/'}).done(function(json){...});
  */
  function zFLT( t, d ){
  	return $.ajax({
