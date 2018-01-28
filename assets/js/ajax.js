81a0b4c53d7550dcd0c382e0b5560d3730696358
Karthik Iyer <kni6228@rit.edu>
2018-01-27 19:43:04 -0500

    Changes: - Used Google APIs to reverse geocode location - Queried to Zomato to get approximate location - Queried to Zomato to get restaurants in location

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,7 +1,6 @@
 /*
  * AJAX function to get information from API
- * two parameters: t, type of request - get or post
- * 				   d, path to get to data in API
+ * two parameters: d, path to get to data in API
  * Example: zFLT('get',{'path':'/[path]/'}).done(function(json){...});
  */
  function zFLT( d ){
@@ -10,6 +9,6 @@
  		async: true,
  		dataType: 'json',
  		url: 'proxy.php',
- 		data: d,
+ 		data: d
  	}); // end ajax function
  } // end myXHR
--- assets/js/script.js
+++ assets/js/script.js
@@ -20,36 +20,67 @@ function getLocation() {
  */
  function getLocationSuccess( position ) {
      
+     // store the latitude and longitude in variables
      var latitude = position.coords.latitude;
      var longitude = position.coords.longitude;
-     var gc = new google.maps.Geocoder();
      var latlng = new google.maps.LatLng( latitude, longitude );
+     // Initialize the Geocoder
+     var gc = new google.maps.Geocoder();
      
+     // Reverse geocode the latitude and longitude
+     // to get an approximate location
      gc.geocode( 
          { 'location': latlng },
-         { 'key': "AIzaSyC4251SJyy4ClZ7NWsumqqG1c0zpui0ViU" },
-         { 'language': "en" },
+         { 'key': "AIzaSyC4251SJyy4ClZ7NWsumqqG1c0zpui0ViU" }, // this is the API Key
+         { 'language': "en" }, // results will be in English
          function( results, status ) {
              switch( status ) {
                  case "OK": 
                      // it worked
+                     var zQuery = {
+                         "path": "/locations/",
+                         "user-key": "1027d5a8e746a4bf1dbc87c98fc6dc0c",
+                         "query": results[ 0 ].sublocality.long_name
+                     }
                      
+                     zFLT( zQuery ).done( 
+                         // get the Zomato formatted location
+                         function( json ) { // do something with it
+                             var zLocation = {
+                                 "path": "/location_details/",
+                                 "user-key": "1027d5a8e746a4bf1dbc87c98fc6dc0c",
+                                 "entity_id": json.entity_id,
+                                 "entity_type": json.entity_type
+                             }
+                             zFLT( zLocation ).done( // get restaurants in the location
+                                 function( json ) {
+                                     
+                                 }
+                             )
+                         }
+                     )                   
                      break;
+                     
                  case "ZERO_RESULTS": 
                      // no results
                      break;
+                     
                  case "OVER_QUERY_LIMIT":
                      // exceeded free query limit
                      break;
+                     
                  case "REQUEST_DENIED":
                      // request was denied :(
                      break;
+                     
                  case "INVALID_REQUEST":
                      // some query is missing
                      break;
+                     
                  default:
                      // unknown or server error
                      break;
+                     
              }
          }
      )
