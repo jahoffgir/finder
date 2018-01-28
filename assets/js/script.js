696254f5edaa8ff7475a2034c6ea08381c22e11a
Karthik Iyer <kni6228@rit.edu>
2018-01-27 23:00:15 -0500

    Changes: - Removed unnecessary reverse geocode functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 /**
- * This script is used for getting the users location
- */
+ * This script gets the user's location and uses the zomato-js-sdk to get 
+ * restaurants near the location
+ *
+ * Sources:
+ * - Google Geolocation API for getting user location
+ *   https://developers.google.com/maps/documentation/geolocation/intro
+ *
+ * - zomato-js-sdk for using Zomato API Library functions
+ *   https://github.com/ameykshirsagar/zomato-js-sdk
+ /
+
+
 /*
  * Function to get location of the user
  */
@@ -29,68 +39,9 @@ function getLocation() {
      
      // Reverse geocode the latitude and longitude
      // to get an approximate location
-     gc.geocode( 
-         { "location": latlng },
-         { "key": "AIzaSyC4251SJyy4ClZ7NWsumqqG1c0zpui0ViU" }, // this is the API Key
-         { "language": "en" }, // results will be in English
-         function( results, status ) {
-             switch( status ) {
-                 case "OK": 
-                     // it worked
-                     var zQuery = {
-                         "path": "/locations/",
-                         "user-key": "1027d5a8e746a4bf1dbc87c98fc6dc0c",
-                         "query": results[ 0 ].sublocality.long_name
-                     }
-                     
-                     zFLT( zQuery ).done( 
-                         // get the Zomato formatted location
-                         function( json ) { // do something with it
-                             var zLocation = {
-                                 "path": "/location_details/",
-                                 "user-key": "1027d5a8e746a4bf1dbc87c98fc6dc0c",
-                                 "entity_id": json.entity_id,
-                                 "entity_type": json.entity_type
-                             }
-                             zFLT( zLocation ).done( // get restaurants in the location
-                                 function( json ) {
-                                     
-                                 }
-                             )
-                         }
-                     )                   
-                     break;
-                     
-                 case "ZERO_RESULTS": 
-                     // no results
-                     alert( "No matches for your location!" );
-                     break;
-                     
-                 case "OVER_QUERY_LIMIT":
-                     // exceeded free query limit
-                     alert( "Exceeded Free API User Limit!" );
-                     break;
-                     
-                 case "REQUEST_DENIED":
-                     // request was denied :(
-                     alert( "Request Denied!" );
-                     break;
-                     
-                 case "INVALID_REQUEST":
-                     // some query is missing
-                     alert( "Invalid Request!" );
-                     break;
-                     
-                 default:
-                     // unknown or server error
-                     alert( "Unknown Server Error!" );
-                     break;
-                     
-             }
-         }
-     )
-     
+    
  }
+
 
 /*
  * Function to handle failure to get current position
@@ -102,8 +53,5 @@ function getLocation() {
 
 /*
  * TO DO
- * 1) Figure out how to get the location name
- * 2) Get user prompt for location access
- * 3) What if location fails, or user denies access?
- *
+ * - Get user prompt for location access
  */
