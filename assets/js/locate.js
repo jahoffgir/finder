e5b2aa5563ac0afda34cf6dc532a5863043369b0
Karthik Iyer <kni6228@rit.edu>
2018-01-28 03:37:40 -0500

    Changes: - Location works accurately - Tested API call - Fixed script tags in index.html

--- assets/js/locate.js
+++ assets/js/locate.js
@@ -31,34 +31,36 @@ function getLocation() {
 /**
  * Function to handle success of getting current position
  */
- function getLocationSuccess( position ) {
-     var latlng = {
-         lat: position.coords.latitude,
-         lon: position.coords.longitude
-     }
-     Zomato.geocode( latlng, function( restaurants ) {
-             // we got the restaurants, what do we do now? 
-             var restaurantObjects = new Array();
-             for( restaurant in restaurants.best_rated_restaurant ) {
-                 restaurantObjects.push( {
-                     name: restaurant.name,
-                     url: restaurant.url,
-                     location: restaurant.location,
-                     cuisine: restaurant.cuisine,
-                     price_range: restaurant.price_range,
-                     user_rating: restaurant.user_rating,
-                     featured_image: restaurant.featured_image,
-                     menu_url: restaurant.menu_url
-                 } );
-             }
-             localStorage.setItem( "restaurants", restaurantObjects );
-         },
-         function( error ) {
-             // life sucks, search failed
-         }
-     );
+function getLocationSuccess( position ) {
+    var latlng = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    }
     
- }
+    Zomato.geocode( latlng, function( restaurants ) {
+        // we got the restaurants, populate and return
+        var restaurantObjects = new Array();
+        var nearbyRestaurants = restaurants.nearby_restaurants;
+        //console.log( restaurants );
+        for( var i = 0; i < Object.keys( nearbyRestaurants ).length; i++ ) {
+            var re = nearbyRestaurants[ i ].restaurant;
+            console.log( re.name );
+            restaurantObjects.push( {
+                name: re.name,
+                url: re.url,
+                location: re.location,
+                cuisine: re.cuisine,
+                price_range: re.price_range,
+                user_rating: re.user_rating,
+                featured_image: re.featured_image,
+                menu_url: re.menu_url
+            } );
+        }
+        localStorage.setItem( "restaurants", restaurantObjects );
+    }, function( error ) {
+        // life sucks, search failed
+    } );
+}
 
 
 /**
@@ -68,10 +70,3 @@ function getLocationFailure() {
      // we need to access user profile's default location
      alert( "Yikes! There was an unknown error in getting your location!" );
 }
-
-
-
-/**
- * TO DO
- * - Get user prompt for location access
- */
