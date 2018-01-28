9d0517f79521041a897940bbd19d607c3aaff31c
Jahongir A <jxa7578@g.rit.edu>
2018-01-28 08:12:32 -0500

    Integrated the restaurant info

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -54,8 +54,6 @@ if (navigator.msMaxTouchPoints) {
 
                 // The movement gets all janky if there's a transition on the elements.
                 $('.animate').removeClass('animate');
-//                document.getElementsByClassName("slide-wrapper")[0].style.display = "block"
-//                document.getElementsByClassName("slide-wrapper")[0].style.display = "block";
             },
 
             move: function(event) {
@@ -66,7 +64,7 @@ if (navigator.msMaxTouchPoints) {
               this.movex = this.index*this.slideWidth + (this.touchstartx - this.touchmovex);
               // Defines the speed the images should move at.
               var panx = 100-this.movex/6;
-              if (this.movex < 800) { // Makes the holder stop moving when there is no more content.
+              if (this.movex < 1000) { // Makes the holder stop moving when there is no more content.
                 this.el.holder.css('transform','translate3d(-' + this.movex + 'px,0,0)');
               }
               if (panx < 100) { // Corrects an edge-case problem where the background image moves without the container moving.
@@ -78,31 +76,49 @@ if (navigator.msMaxTouchPoints) {
 
             end: function(event) {
                 
-              // Calculate the distance swiped.
-              var absMove = Math.abs(this.index*this.slideWidth - this.movex);
-              // Calculate the index. All other calculations are based on the index.
-              if (absMove > this.slideWidth/2 || this.longTouch === false) {
-                if (this.movex > this.index*this.slideWidth && this.index < 2) {
-                  this.index++;
-                   
-                } else if (this.movex < this.index*this.slideWidth && this.index > 0) {
-                  this.index--;
-                   
-//                    alert("BYEEE")
+                // Calculate the distance swiped.
+                var absMove = Math.abs(this.index*this.slideWidth - this.movex);
+                // Calculate the index. All other calculations are based on the index.
+                if (absMove > this.slideWidth/2 || this.longTouch === false) {
+                    if (this.movex > this.index*this.slideWidth && this.index < 2) {
+                        this.index++; 
+                    } else if (this.movex < this.index*this.slideWidth && this.index > 0) {
+                        this.index--;
+                    }
+                }      
+                // Move and animate the elements.
+                this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
+                this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
+                this.index = 1;
+                var i;
+                var myNode = document.getElementsByClassName('slide-image');
+                
+                for (i = 0; i < document.getElementsByClassName('slide-image').length; i++) {
+                    document.getElementsByClassName('slide-image')[i].parentNode.removeChild(myNode[i]);
                 }
-              }      
-              // Move and animate the elements.
-              this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
-              this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + 100-this.index*50 + 'px,0,0)');
-//                document.getElementsByClassName("slide-wrapper")[0].style.display = "block"
-
+                if (document.getElementsByClassName('slide-image').length) {
+                    document.getElementsByClassName('slide-image')[0].parentNode.removeChild(myNode[0]);
+                }
+                
+                for (i = 0; i < 3; i++) {
+                    var el = document.createElement("img");
+                    el.setAttribute("class", "slide-image");
+                    el.setAttribute("src", " ");
+                    document.getElementsByClassName('slide')[0].parentNode.appendChild(el);
+                }
+                indx = localStorage.getItem("curser");
+                arr = localStorage.getItem("restaurants");
+                // Adds a different restaurant pic
+                document.getElementsByClassName('slide-image')[1].setAttribute("src", arr[indx].featured_image  );
+                localStorage.setItem("curser", indx + 1);
+                // timeout for the image replacement
+                window.setTimeout(function() {
+                    location.reload(slider.js);
+                }, 700)
+                
             }
 
             };
             document.getElementsByClassName("slide-wrapper")[0].style.display = "none";
             slider.init();
-//            document.getElementsByClassName("slide-wrapper")[0].style.display = "none"
-//            this.el.holder.addClass('animate').css('transform', 'translate3d(-' + this.index*this.slideWidth + 'px,0,0)');
-//            this.el.imgSlide.addClass('animate').css('transform', 'translate3d(-' + -100-this.index*50 + 'px,0,0)');
-         
         }
